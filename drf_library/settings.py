4664546cb260f7bb6cb11f530556150ccab99031
Alex <a9p9m9@gmail.com>
2025-11-14 00:09:52 +0200

    add DEFAULT_PERMISSION_CLASSES to project

--- drf_library/settings.py
+++ drf_library/settings.py
@@ -135,6 +135,9 @@ INTERNAL_IPS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.AllowAny",
     )
 }
 
