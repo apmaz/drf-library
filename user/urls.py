117602ea724e55350392053c8b0ce86483f16b6b
Alex <a9p9m9@gmail.com>
2025-11-14 00:16:25 +0200

    update views.py to ManageUserView, add permissions and route for me page

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView
 )
 
-from user.views import CreateUserView
+from user.views import CreateUserView, ManageUserView
 
 
 app_name = "user"
@@ -13,6 +13,7 @@ app_name = "user"
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="register"),
+    path("me/", ManageUserView.as_view(), name="manage_user"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
