1f13705a56539d52850090443259c3e20ee518fc
Alex <a9p9m9@gmail.com>
2025-11-17 15:11:15 +0200

    add serializers for Borrow model

---
+++ borrow/serializers.py
@@ -0,0 +1,52 @@
+from rest_framework import serializers
+from borrow.models import Borrow
+from book.serializers import BookSerializer
+from user.serializers import UserSerializer
+
+
+class BorrowListSerializer(serializers.ModelSerializer):
+    book = serializers.StringRelatedField(read_only=True)
+    user = serializers.SlugRelatedField(
+        slug_field="email",
+        read_only=True,
+    )
+
+    class Meta:
+        model = Borrow
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
+
+
+class BorrowRetrieveSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Borrow
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
+
+
+class BorrowSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Borrow
+        fields = (
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
