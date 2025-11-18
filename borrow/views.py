3fb3c39e137833a4a89c1317cff4c7657edc6be5
Alex <a9p9m9@gmail.com>
2025-11-18 14:22:58 +0200

    override get_queryset in BorrowViewSet: admins can sees all Borrow objects, non admin can sees only personal objects

--- borrow/views.py
+++ borrow/views.py
@@ -18,6 +18,12 @@ class BorrowViewSet(
     queryset = Borrow.objects.all()
     permission_classes = (IsAuthenticated,)
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.is_staff:
+            return queryset
+        return queryset.filter(user=self.request.user)
+
     def get_serializer_class(self):
         if self.action == "list":
             return BorrowListSerializer
