29352df692d019c0940a25cc0b9fe0a0b95ccc1f
Alex <a9p9m9@gmail.com>
2025-11-26 00:00:37 +0200

    implement send_borrow_created_message func for sending message to telegram

---
+++ notifications/telegram_services.py
@@ -0,0 +1,38 @@
+import os
+import requests
+from dotenv import load_dotenv
+from borrow.models import Borrow
+
+
+def send_borrow_created_message(instance: Borrow) -> None:
+    load_dotenv()
+    bot_token = os.getenv("BOT_TOKEN")
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+
+    payload = {
+        "text": f"Created a new Borrow:\n\n"
+                f"- Borrow ID: {instance.id},\n"
+                f"- Borrow create date: {instance.borrow_date},\n"
+                f"- Expected return date: {instance.expected_return_date},\n\n"
+                f"- Book ID: {instance.book.id},\n"
+                f"- Book title: {instance.book.title},\n"
+                f"- Book author: {instance.book.author},\n\n"
+                f"- User ID: {instance.user.id},\n"
+                f"- User email: {instance.user.email},\n"
+                f"- User first name: {instance.user.first_name},\n"
+                f"- User last name: {instance.user.last_name}",
+        "parse_mode": "",
+        "disable_web_page_preview": False,
+        "disable_notification": False,
+        "reply_to_message_id": None,
+        "chat_id": os.getenv("TELEGRAM_CHAT_ID")
+    }
+
+    headers = {
+        "accept": "application/json",
+        "User-Agent": "Telegram Bot SDK - (https://github.com/irazasyed/telegram-bot-sdk)",
+        "content-type": "application/json"
+    }
+
+    requests.post(url, json=payload, headers=headers)
