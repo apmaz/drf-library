7dc8845c0a929e543a5227171c62b1cc7afcdcdb
Alex <a9p9m9@gmail.com>
2025-11-26 19:11:40 +0200

    add routing for Payment model in payments app

---
+++ payments/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+from payments.views import PaymentViewSet
+
+
+app_name = "payment"
+
+router = routers.DefaultRouter()
+router.register("payments", PaymentViewSet, basename="payment")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
