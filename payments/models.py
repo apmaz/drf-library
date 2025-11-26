2463302d021a697886d64e2fd8a33fb1caad5671
Alex <a9p9m9@gmail.com>
2025-11-26 18:37:55 +0200

    add Payment model to payments app

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from borrow.models import Borrow
 
-# Create your models here.
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "pending"
+        PAID = "paid"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "payment"
+        FINE = "fine"
+
+    status = models.CharField(
+        choices=StatusChoices.choices, default=StatusChoices.PENDING
+    )
+    type = models.CharField(
+        choices=TypeChoices.choices, default=None, null=True, blank=True
+    )
+    borrowing = models.ForeignKey(
+        Borrow, on_delete=models.CASCADE, related_name="borrow"
+    )
+    session_url = models.URLField(max_length=255, null=True, blank=True)
+    session_id = models.CharField(max_length=255, null=True, blank=True)
+    money_to_pay = models.DecimalField(max_digits=6, decimal_places=3)
