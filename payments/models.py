3cf88dcf79245002ecdda4b256b054d3673ddaf5
Alex <a9p9m9@gmail.com>
2025-12-03 07:42:11 +0200

    change related_name for borrowin feald on 'payments' and add __str__ method for Payment

--- payments/models.py
+++ payments/models.py
@@ -18,8 +18,11 @@ class Payment(models.Model):
         choices=TypeChoices.choices, default=None, null=True, blank=True
     )
     borrowing = models.ForeignKey(
-        Borrow, on_delete=models.CASCADE, related_name="borrow"
+        Borrow, on_delete=models.CASCADE, related_name="payments"
     )
     session_url = models.URLField(max_length=255, null=True, blank=True)
     session_id = models.CharField(max_length=255, null=True, blank=True)
     money_to_pay = models.DecimalField(max_digits=6, decimal_places=3)
+
+    def __str__(self):
+        return f"ID: {self.pk}, borrow_id: {self.borrowing.id}, Money_to_pay: {self.money_to_pay}"
