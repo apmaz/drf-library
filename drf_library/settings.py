b14015db51666cbe23d32cb208f5a450399e3693
Alex <a9p9m9@gmail.com>
2025-12-22 11:42:22 +0200

    - implement PostgrSQL to the project - update .env.sample - update requirements.txt

--- drf_library/settings.py
+++ drf_library/settings.py
@@ -87,17 +87,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "drf_library.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT", "5432"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
