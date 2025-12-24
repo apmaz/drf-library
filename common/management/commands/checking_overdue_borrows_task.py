84e84e1c3d76ce7dc632d5805f82a4697e8d5acf
Alex <a9p9m9@gmail.com>
2025-12-24 13:09:17 +0200

    implemet 'wait_for_db' and 'checking_overdue_borrows_task' castom comands

---
+++ common/management/commands/checking_overdue_borrows_task.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand
+from django_q.models import Schedule
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        schedule, created = Schedule.objects.get_or_create(
+            func="borrow.borrow_services.checking_overdue_borrows",
+            schedule_type=Schedule.MINUTES,
+            repeats=-1,
+        )
+        if created:
+            self.stdout.write(self.style.SUCCESS("Schedule created successfully"))
+        else:
+            self.stdout.write(self.style.WARNING("Schedule already exists"))
